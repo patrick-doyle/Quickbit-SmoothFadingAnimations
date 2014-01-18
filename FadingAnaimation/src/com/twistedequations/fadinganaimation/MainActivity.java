9da891431d00b6f5178470e49bbf166b2a91ec53
Patrick Doyle <patrick@twsitedequations.com>
2014-01-18 15:27:46 +0000

    Initial commit

---
+++ FadingAnaimation/src/com/twistedequations/fadinganaimation/MainActivity.java
@@ -0,0 +1,46 @@
+package com.twistedequations.fadinganaimation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final View view = findViewById(R.id.fadeOutLayer);
+		Animation fadeOut = new AlphaAnimation(0f, 1f);
+		fadeOut.setDuration(3000);
+		fadeOut.setAnimationListener(new AnimationListener() {
+			
+			@Override
+			public void onAnimationStart(Animation animation) {}
+			
+			@Override
+			public void onAnimationRepeat(Animation animation) {}
+			
+			@Override
+			public void onAnimationEnd(Animation animation) {
+				view.setVisibility(View.VISIBLE);
+				
+			}
+		});
+		
+		view.startAnimation(fadeOut);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
